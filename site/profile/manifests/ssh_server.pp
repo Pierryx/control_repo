1632253e1927637201bf69283aa0272eb1440849
Pierryx <110187343+Pierryx@users.noreply.github.com>
2022-07-29 15:51:58 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -9,7 +9,7 @@ class profile::ssh_server {
   ssh_authorized_key { 'root@master.puppet.vm':
           ensure => present,
           user => 'root',
-          type => 'ecdsa-sha2-nistp521'
+          type => 'ecdsa-sha2-nistp521',
           key  => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAHxAL35f2EuAMMSkcXxRIQ7DnuJV4BFlefoqWSPnr3QeRbi3Uirye05qQL4ma4+VFslOh40PAWXTF3hgjzpjIPgtwFH6Fgm7kW5BeDjONmmiZj9D0nU98cN8Y8Y3kOeqIydz0Mcgd1DDBZUGPvg04cS6odti3548kVd3MuLk/NEuXXj+w==',
   }
 }
