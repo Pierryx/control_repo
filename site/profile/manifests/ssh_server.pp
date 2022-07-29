1b5c9ebb1bf4d3424fc0951626ecc800226d3835
Pierryx <110187343+Pierryx@users.noreply.github.com>
2022-07-29 15:46:15 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,4 +12,5 @@ class profile::ssh_server {
           type => 'ecdsa-sha2-nistp521'
           key  => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAHxAL35f2EuAMMSkcXxRIQ7DnuJV4BFlefoqWSPnr3QeRbi3Uirye05qQL4ma4+VFslOh40PAWXTF3hgjzpjIPgtwFH6Fgm7kW5BeDjONmmiZj9D0nU98cN8Y8Y3kOeqIydz0Mcg
 d1DDBZUGPvg04cS6odti3548kVd3MuLk/NEuXXj+w==',
+  }
 }
